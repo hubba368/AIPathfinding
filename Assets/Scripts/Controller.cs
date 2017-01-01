9eb2ee0a44df58e09c821032c12be9184d458f91
hubba368 <elliott_bowman@hotmail.com>
2017-01-01 21:52:35 +0000

    Added randomised spawning of objects and improved camera control

--- Assets/Scripts/Controller.cs
+++ Assets/Scripts/Controller.cs
@@ -3,39 +3,75 @@ using System.Collections;
 
 public class Controller : MonoBehaviour
 {
+    public enum RotationAxes {  MouseXandY = 0, MouseX = 1, MouseY = 2}
+    public RotationAxes axes = RotationAxes.MouseXandY;
+
+    float speed = 15f;
+    float rotateY = 0f;   
+    float scrollSpeed = 10f;
 
 
-
-    Camera mainCam;
-
-    float speed = 6;
-   // float zoomSpeed = 3;
-    
-    int scrollDist = 5;
-    float scrollSpeed = 10;
 
     //Camera Controls
-    void Update ()
+    void KeyboardInput()
     {
-        float mousePosX = Input.mousePosition.x;
-        float mousePosY = Input.mousePosition.y;
-        //RTS style camera movement
-        if (mousePosX < scrollDist)
+        //WASD camera movement
+        if (Input.GetKey(KeyCode.A))
         {
+            //left
             transform.Translate(Vector3.right * -scrollSpeed * Time.deltaTime);
         }
-        else if(mousePosX >= Screen.width - scrollDist)
+        else if (Input.GetKey(KeyCode.D))
         {
+            //right
             transform.Translate(Vector3.right * scrollSpeed * Time.deltaTime);
         }
-        else if(mousePosY < scrollDist)
+        else if (Input.GetKey(KeyCode.S))
         {
+            //down
             transform.Translate(Vector3.forward * -scrollSpeed * Time.deltaTime);
         }
-        else if (mousePosY >= Screen.height - scrollDist)
+        else if (Input.GetKey(KeyCode.W))
         {
+            //up
             transform.Translate(Vector3.forward * scrollSpeed * Time.deltaTime);
         }
+
+    }
+
+    void MouseControl()
+    {
+        //Controls view of camera in first person
+        if(axes == RotationAxes.MouseXandY)
+        {
+            //if both axes are being moved/rotated
+            float rotateX = transform.localEulerAngles.y + Input.GetAxis("Mouse X") * speed;   //rotate X axis
+
+            rotateY += Input.GetAxis("Mouse Y") * speed;
+            rotateY = Mathf.Clamp(rotateY, -60f, 60f);   //clamp to stop from looping all the way around
+
+            transform.localEulerAngles = new Vector3(-rotateY, rotateX, 0);   //rotate Y axis
+        }
+        else if (axes == RotationAxes.MouseX)
+        {
+            //rotate X axis
+            transform.Rotate(0, Input.GetAxis("Mouse X") * speed, 0);
+        }
+        else
+        {
+            //rotateY Y axis
+            rotateY += Input.GetAxis("Mouse Y") * speed;
+            rotateY = Mathf.Clamp(rotateY, -60f, 60f);
+
+            transform.localEulerAngles = new Vector3(-rotateY, transform.localEulerAngles.y, 0);
+        }
+    }
+
+
+    void Update ()
+    {
+        MouseControl();
+        KeyboardInput();
     }
 
 }
--- Assets/Scripts/CreateWorld.cs
+++ Assets/Scripts/CreateWorld.cs
@@ -4,12 +4,16 @@ using System.Collections;
 public class CreateWorld : MonoBehaviour
 {
     public Vector3 spawnPoint;
-    public GameObject actor;
-    public int maxActors = 30;
+    public Quaternion rotation;
+
+    public GameObject[] actors;
+    public int maxActors = 30;  //max amount that can be spawned
+
 
     Vector3 GetRandomSpawnPoint()
     {
-        Vector3 spawnRadius = Random.insideUnitSphere * 500f;
+        //Generate a random spawn point by using insideUnitSphere
+        Vector3 spawnRadius = Random.insideUnitSphere * 75f;
         spawnRadius.y = 0.0f;
 
         spawnPoint = spawnRadius + transform.position;
@@ -19,16 +23,24 @@ public class CreateWorld : MonoBehaviour
 
     void SpawnObject()
     {
+        //Spawn objects that are set in the inspector
         int spawned = 0;
+        
+
+        float direction = Random.Range(0f, 360f);
 
         while(spawned < maxActors)
         {
-            Instantiate(actor, GetRandomSpawnPoint(), Quaternion.identity);
+            //spawn until limit reached
+            int prefab = Random.Range(0, 3);
+            Instantiate(actors[prefab], GetRandomSpawnPoint(), Quaternion.Euler(new Vector3(0f, direction, 0f)));
+            spawned++;
         }  
     }
 
     void Start()
     {
+        
         SpawnObject();
     }
 	
